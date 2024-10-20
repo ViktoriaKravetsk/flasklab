ce2cd6f4c8e43f0df8e4812511c6ce69e684ffdb
Viktoriia <viktorialravecka@gmail.com>
2024-10-21 00:00:34 +0300

    to_url

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Hello, Flask!'
+def hello():
+    return "Hello, World!", 200
 
-if __name__ == '__main__':
+@app.route('/homepage')
+def homepage():
+    agent = request.user_agent
+    return f"Welcome to the homepage! - {agent}"
+
+@app.route("/hi/<string:name>")
+def greetings(name):
+    name = name.upper()
+    age = request.args.get("age", default=0, type=int)
+    year = 2024 - age
+    return f"Welcome, {name} - {year}"
+
+@app.route('/admin')
+def admin():
+    to_url = url_for("greetings", name="administrator", age=45, _external=True)
+    print(to_url)
+    return redirect(to_url)
+
+if __name__ == '__main__':  # Змінено name на __name__
     app.run(debug=True)
